2fba989099393b688696046a76b323eec7e9e94d
Paulo Meirelles <paulo.meirelles@tabas.com.br>
2022-11-20 11:27:49 -0300

    Tests NOK

--- queries.js
+++ queries.js
@@ -1,11 +1,12 @@
 const Pool = require("pg").Pool;
-const secretkeys = require("./secretkeys");
+require("dotenv").config({ path: "./secretKeys.env" });
+
 const pool = new Pool({
-  user: secretkeys.user,
-  host: secretkeys.host,
-  database: secretkeys.database,
-  password: secretkeys.password,
-  port: secretkeys.port,
+  user: process.env.user,
+  host: process.env.host,
+  database: process.env.database,
+  password: process.env.password,
+  port: process.env.port,
 });
 
 const getConta = (request, response) => {
